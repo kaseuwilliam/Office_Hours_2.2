ed884282a4519aec6dd5b3b4969c6c1c5d8d35c2
William Kaseu <wkaseu@seas.upenn.edu>
2023-06-09 19:55:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const prompt = require('prompt-sync')();
+
+// if statement to print "Happy Friday", if it is a Friday 
+// if statement to print "It is the weekend", if it is a Saturday or Sunday 
+// if statement to print "This is a normal weekday", if it Mon, Tues, Wed, or Thur 
+
+// let day = "Monday";
+
+// if (day === "Friday"){
+
+//     console.log("Happy Friday");
+
+// } else if (day === "Saturday" || day === "Sunday") {
+
+//     console.log("It is the weekend");
+
+// } else if (day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday") {
+
+//     console.log("This is a normal weekday");
+
+// } else{
+
+//     console.log("You inputted an invalid day of the week");
+// }
+
+
+
+// let age = parseInt(prompt("What is your age? "));
+
+// while(age < 21){
+
+//     console.log(`You are ${age} years old. Please come back in a year`);
+
+//     // age = age + 1;
+//     // age += 1;
+//     age++;
+
+// }
+
+
+
+let correctAnswer = 123;
+let guess = parseInt(prompt("What is your guess? "));
+
+let numberOfGuesses = 0;
+
+while(correctAnswer != guess && numberOfGuesses <= 5){
+
+    // continue playing the game
+    console.log("Hi, you guessed incorrectly. Please try again.");
+    guess = parseInt(prompt("What is your guess? "));
+
+    numberOfGuesses++;
+
+}
+
+if (numberOfGuesses < 6){
+
+    console.log("Congrats, you guessed correctly");
+} else{
+    console.log("Unfortunately, you have LOST");
+}
+
+
+
+
